34b889e917477bdba5b056a5ada18076734a7bb5
Nicha Ngamtweerat <nicha_nga@hotmail.com>
2020-06-17 16:23:09 +0700

    renamed and reordered folders

--- react-projects/main.js
+++
@@ -1,9 +0,0 @@
-import {sayHi, sayBye} from './say.js';
-import {User} from './user.js';
-
-sayHi('John'); // Hello, John!
-sayBye('John'); // Bye, John!import {User} from './user.js';
-
-let user = new User();
-user.name = 'Nicha';
-console.log('Hello ' + user.name);
