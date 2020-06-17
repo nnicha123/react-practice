f0c455ac67c114817cf947ba077cb6586bfcdf64
Nicha Ngamtweerat <nicha_nga@hotmail.com>
2020-06-17 09:22:10 +0700

    succesfully added the remove button

--- JS_for_react/react-records/src/ClassCom.js
+++ JS_for_react/react-records/src/ClassCom.js
@@ -1,35 +1,63 @@
 import React, { Component } from 'react';
-
+import './App.css'
 class Class1 extends React.Component {
     state = {
         dataNew: '',
         count: 0,
         color: true,
-        text: '',
+        textData:{
+            text: '',
+            key:''
+        },
         data: []
     }
     submit = (e) => {
         e.preventDefault();
-        let move = [...this.state.data, this.state.text]
-        this.setState({ data: move,text:'' });
-        document.querySelector('input').value = '';
+        const newItem = this.state.textData
+        if(newItem.text.length > 0){
+            const move = [...this.state.data, newItem]
+            this.setState({ data: move,textData:{text:"",key:""}});
+        }
     }
+    add = () =>{
+        this.setState({
+            count:this.state.count+1
+        })
+    }
+    subtract = () => {
+        this.setState({
+            count:this.state.count-1
+        })
+    }
+    reset = () => {
+        this.setState({
+            count:0
+        })
+    }
+    toggleColor = () => {
+        this.setState({ color: !this.state.color })
+    }
+    remove = (key) => {
+        const newList = this.state.data.filter(el => el.key !== key)
+        this.setState({data:newList});
+    }
+
     render() {
         let { name, age } = this.props;
         return (
             <div>
                 {/* <div style={{ fontSize: "20px", color: "green" }}>Class component {this.props.name} {this.props.age}</div> */}
-                <h1 onClick={() => this.setState({ color: !this.state.color })} style={{ color: this.state.color ? "tomato" : 'blue' }}>Click me</h1>
+                <h1 onClick={this.toggleColor} style={{ color: this.state.color ? "tomato" : 'blue' }}>Click me</h1>
                 <p>{this.state.dataNew}</p>
-                <button onClick={() => this.setState({ count: this.state.count + 1 })}>+</button>
-                <button onClick={() => this.setState({ count: this.state.count - 1 })}>-</button>
-                <button onClick={() => this.setState({ count: 0 })}>Reset</button>
-                <h3 onClick={() => this.setState({ color: !this.state.color })} style={{ color: this.state.color ? "tomato" : 'blue' }}>{this.state.count}</h3>
+                <button onClick={this.add}>+</button>
+                <button onClick={this.subtract}>-</button>
+                <button onClick={this.reset}>Reset</button>
+                <h3 onClick={this.toggleColor} style={{ color: this.state.color ? "tomato" : 'blue' }}>{this.state.count}</h3>
                 <p>{this.newcount}</p>
                 <form onSubmit={this.submit}>
-                    <input type="text" onChange={e => { this.setState({ text: e.target.value }) }} />
+                    <input type="text" value={this.state.textData.text} onChange={e => { this.setState({ textData:{text: e.target.value, key:Date.now()} }) }} />
                     <ul>
-                        {this.state.data.map(element => <li>{element}</li>)}
+                        {this.state.data.map(element => <li className="listStyle" key={element.key}><span>{element.text}</span><span><button onClick={() => this.remove(element.key)}>Remove</button></span></li>)}
                     </ul>
                     <button type="submit" onSubmit={this.submit}>Submit</button>
                 </form>
