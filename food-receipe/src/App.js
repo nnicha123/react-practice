5d9f1873b8a7c19dc903e0197dbdd5957baa488c
Nicha Ngamtweerat <nicha_nga@hotmail.com>
2020-06-17 15:55:04 +0700

    rename folder

---
+++ food-receipe/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Hello</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
