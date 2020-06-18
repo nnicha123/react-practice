203458b3152d7a81679db9f9f282d7c04551264d
Nicha Ngamtweerat <nicha_nga@hotmail.com>
2020-06-18 11:38:22 +0700

    renamed folders

--- react-projects/ant-design-form/src/App.js
+++ react-projects/ant-design-form/src/App.js
@@ -1,74 +1,55 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { Table, Row, Col } from 'antd';
+import { Row, Col, Form, Input, Button } from 'antd';
+import _default from 'antd/lib/time-picker';
 
 function App() {
-  const students = [{
-    "first_name": "Nona",
-    "last_name": "St. Hill",
-    "email": "nsthill0@miitbeian.gov.cn",
-    "gender": "Female",
-    "university": "Istanbul Arel University"
-  }, {
-    "first_name": "Felice",
-    "last_name": "Wilcocke",
-    "email": "fwilcocke1@nytimes.com",
-    "gender": "Male",
-    "university": "University of Maryland Baltimore County"
-  }, {
-    "first_name": "Chester",
-    "last_name": "Ruske",
-    "email": "cruske2@spotify.com",
-    "gender": "Male",
-    "university": "Rafsanjan University of Medical Sciences"
-  }, {
-    "first_name": "Christye",
-    "last_name": "Biddy",
-    "email": "cbiddy3@sakura.ne.jp",
-    "gender": "Female",
-    "university": "Universidad Cientifica Latinoamericana de Hidalgo"
-  }, {
-    "first_name": "Naoma",
-    "last_name": "Bithell",
-    "email": "nbithell4@is.gd",
-    "gender": "Female",
-    "university": "Kashan University of Medical Sciences"
-  }, {
-    "first_name": "Adore",
-    "last_name": "Spaldin",
-    "email": "aspaldin5@wunderground.com",
-    "gender": "Female",
-    "university": "Canisius College"
-  }]
-  const columns = [
-    {
-      title: "Email",
-      dataIndex: "email"
+  const layout = {
+    labelCol: {
+      xs: 6,
+      sm: 8,
+      md: 10
     },
-    {
-      title: "Gender",
-      dataIndex: "gender"
-    },
-    {
-      title: "First Name",
-      dataIndex: "first_name",
-    },
-    {
-      title: "Surname",
-      dataIndex: "last_name",
-    },
-    {
-      title: "University",
-      dataIndex: "university",
+    wrapperCol: {
+      xs: 18,
+      sm: 16,
+      md: 14
     },
 
-  ]
+  }
+  let onFinish = (val) => {
+    console.log(val)
+  }
   return (
     <div className="App">
       <Row justify="center">
         <Col xs={24} sm={24} md={20} lg={16} xl={14} xxl={12}>
-          <Table bordered={true} size={"middle"} columns={columns} dataSource={students} />
+          <Form onFinish={onFinish} {...layout}>
+            <Form.Item label="Name" name="name"
+              rules={[{ required: true, message: "Name is Required" }, { max: 6, min: 3, message: "Needs to be between 3-6" }]}>
+              <Input placeholder="Name" />
+            </Form.Item >
+            <Form.Item label="PhoneNumber" name="phoneNumber"
+              rules={[
+                { len: 10, message: "Number incorrect" }
+              ]}>
+              <Input placeholder="Phone Number" />
+            </Form.Item>
+            <Form.Item label="Email" name="email"
+              rules={[{ required: true, message: "Email is required" },
+              { type: 'email', message: "Must be of type email" }
+              ]}>
+              <Input placeholder="Email" type="email" />
+            </Form.Item >
+            <Form.Item label="Password" name="password"
+              rules={[{ required: true, message: "Password is required" },
+              { min: 12, max: 24, message: "Password needs to be between 12-24 characters" }
+              ]}>
+              <Input placeholder="Password" type="password"/>
+            </Form.Item>
+            <Button htmlType="submit">Register</Button>
+          </Form>
         </Col>
       </Row>
     </div>
---
+++ react-projects/life-cycle/src/Components/Article.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react'
+
+export class Article extends Component {
+    static getDerivedStateFromProps(props,state){
+        console.log("[Article.js] getDerivedStateFromProps")
+        return state;
+    }
+    shouldComponentUpdate(nextProps,nextState){
+        console.log("[Article.js] shouldComponentUpdate");
+        return true
+    }
+    getSnapshotBeforeUpdate(prevProps,prevState){
+        console.log("[Article.js] getSnapshotBeforeUpdate");
+        return {x:0,y:0}
+    }
+    componentDidUpdate(prevProps,prevState,snapshot){
+        console.log("[Article.js] componentDidUpdate",snapshot);
+    }
+    render() {
+        console.log("[Article.js] Render")
+        return (
+            <div>
+                {this.props.article}
+            </div>
+        )
+    }
+}
+
+export default Article
