8a7fcb74f713c9f20f96dd225a4c62eac7722fbf
Nicha Ngamtweerat <nicha_nga@hotmail.com>
2020-06-18 21:39:38 +0700

    can now successfully swap

--- react-projects/mini-project/src/TestComp.js
+++ react-projects/mini-project/src/TestComp.js
@@ -4,25 +4,40 @@ import './TestComp.css';
 export class TestComp extends Component {
     state = {
         arr: [1, 20, 14, 2, 3, 5, 10],
-        countIndex:0
+        storeArr : [1,20,14,2,3,5,10],
+        countIndex: 0
     }
     swap = () => {
-       let newArr = [1,14,20,2,3,5,10];
-       console.log(this.state.arr,newArr)
-       this.setState({arr:newArr,countIndex:this.state.countIndex+1});
-       console.log(this.state.countIndex)
+        let newArr = [...this.state.arr];
+        console.log(this.state.countIndex, newArr.length)
+        if (this.state.countIndex > newArr.length -2) {
+            this.setState({ countIndex:0 })
+            console.log('In the loop', this.state.countIndex)
+        }
+        else {
+            if (newArr[this.state.countIndex] > newArr[this.state.countIndex + 1]) {
+                [newArr[this.state.countIndex], newArr[this.state.countIndex + 1]] = [newArr[this.state.countIndex + 1], newArr[this.state.countIndex]]
+            }
+            this.setState({ arr: newArr, countIndex: this.state.countIndex + 1 });
+            console.log(this.state.countIndex)
+        }
+    }
+    reset = () =>{
+        let newArr = [...this.state.storeArr];
+        this.setState({arr:newArr,countIndex:0});
     }
     render() {
         return (
             <div className="container">
                 <ul className="listUl">
-                    {this.state.arr.map((el,index) =>
-                        <li key={index+1}>
+                    {this.state.arr.map((el, index) =>
+                        <li key={index + 1}>
                             <div className="topElement" style={{ height: el * 10 }}></div>
                             <div>{el}</div>
                         </li>)}
                 </ul>
                 <button onClick={this.swap}>Swap</button>
+                <button onClick={this.reset}>Reset</button>
             </div>
         )
     }
