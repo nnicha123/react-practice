aa3b7ea3ff050f0bcf929a1852369078a8fab896
Nicha Ngamtweerat <nicha_nga@hotmail.com>
2020-06-19 14:08:38 +0700

    use reducer examples react hooks

---
+++ react-projects/hooks-usereducer/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import CounterOne from './CounterOne';
+import CounterTwo from './CounterTwo';
+import CounterThree from './CounterThree';
+
+function App() {
+  return (
+    <div className="App">
+      {/* <CounterOne/> */}
+      {/* <CounterTwo/> */}
+      <CounterThree/>
+    </div>
+  );
+}
+
+export default App;
