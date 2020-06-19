6b766df6e4222293e05633c80e033d8a0fd5ed89
Nicha Ngamtweerat <nicha_nga@hotmail.com>
2020-06-19 14:49:33 +0700

    useReducer and useContext used together to manage count project

--- react-projects/hooks-usereducer-usecontext/src/App.js
+++ react-projects/hooks-usereducer-usecontext/src/App.js
@@ -1,25 +1,40 @@
-import React from 'react';
+import React, { useReducer } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import ComponentA from './ComponentA';
+import ComponentB from './ComponentB';
+import ComponentC from './ComponentC';
+// Flow
+// App -> A
+// App -> B -> D
+// App -> C -> E -> F
+export const CountContext = React.createContext()
+
+const initialState = 0
+const reducer = (state, action) => {
+  switch (action) {
+    case 'increment':
+      return state + 1
+    case 'decrement':
+      return state - 1
+    case 'reset':
+      return initialState
+    default:
+      return state
+  }
+}
 
 function App() {
+  const [count, dispatch] = useReducer(reducer, initialState)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <CountContext.Provider value={{countState:count,countDispatch:dispatch}}>
+      <div className="App">
+        Count {count}
+        <ComponentA />
+        <ComponentB />
+        <ComponentC />
+      </div>
+    </CountContext.Provider>
   );
 }
 
