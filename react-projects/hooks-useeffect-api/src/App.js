718846a7626f061e0a05eeb945838d8d3a30a0e7
Nicha Ngamtweerat <nicha_nga@hotmail.com>
2020-06-19 13:00:02 +0700

    fetching api data using useeffect

---
+++ react-projects/hooks-useeffect-api/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import DataFetching from './DataFetching';
+import DataFetchingTwo from './DataFetchingTwo';
+
+function App() {
+  return (
+    <div className="App">
+      {/* <DataFetching/> */}
+      <DataFetchingTwo/>
+    </div>
+  );
+}
+
+export default App;
