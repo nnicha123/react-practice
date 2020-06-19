58343c1a368381f5bc61b91f3e03f76cd7830175
Nicha Ngamtweerat <nicha_nga@hotmail.com>
2020-06-19 14:16:46 +0700

    commited added file

--- react-projects/todo/src/App1.js
+++ react-projects/todo/src/App1.js
@@ -3,6 +3,6 @@ import React from 'react';
 function App1(props){
     let {data,send,name} = props
     console.log(props.data)
-    return(<div>Data: {props.data}, Send : {props.send}, Name : {props.name}</div>)
+    return(<div>Data: {data}, Send : {send}, Name : {name}</div>)
 }
 export default App1;
