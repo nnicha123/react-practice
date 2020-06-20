2e980f21a3715bbeae9fe4ec1446a4c69ca6939b
Nicha Ngamtweerat <nicha_nga@hotmail.com>
2020-06-20 13:11:13 +0700

    modify calculator now if click on operation button will do the operation

--- react-projects/proj-react-router/src/Calculator.js
+++ react-projects/proj-react-router/src/Calculator.js
@@ -2,50 +2,63 @@ import React, { Component } from 'react'
 import './Calculator.css'
 class Calculator extends Component {
     state = {
-        result: 0
+        result: 0,
+        count: 5
     }
+
+    operationCheck = () => {
+        if (this.state.result.includes('+')) {
+            let operator = this.state.result.indexOf('+')
+            let firstNum = this.state.result.substr(0, operator);
+            let secondNum = this.state.result.substr(operator + 1, this.state.result.length)
+            this.setState({ result: Number(firstNum) + Number(secondNum) })
+        }
+        else if (this.state.result.includes('-')) {
+            let operator = this.state.result.indexOf('-')
+            let firstNum = this.state.result.substr(0, operator);
+            let secondNum = this.state.result.substr(operator + 1, this.state.result.length)
+            this.setState({ result: (Number(firstNum) - Number(secondNum)) })
+        }
+        else if (this.state.result.includes('÷')) {
+            let operator = this.state.result.indexOf('÷')
+            let firstNum = this.state.result.substr(0, operator);
+            let secondNum = this.state.result.substr(operator + 1, this.state.result.length)
+            this.setState({ result: (Number(firstNum) / Number(secondNum)) })
+        }
+    }
+
     handleClick = (val) => {
-        if (val == 'clear') {
+        console.log(this.state.count)
+        if (this.state.count === 1){
+            this.operationCheck()
+            this.setState({count : 5})
+        }
+        else if (val == 'clear') {
             this.setState({ result: 0 })
         } else if (val === '=') {
-            console.log(this.state.result)
-            // console.log(this.state.result.includes('+'))
-            if (this.state.result.includes('+')) {
-                let operator = this.state.result.indexOf('+')
-                let firstNum = this.state.result.substr(0, operator);
-                let secondNum = this.state.result.substr(operator + 1, this.state.result.length)
-                this.setState({result:Number(firstNum) + Number(secondNum)})
-            }
-            else if (this.state.result.includes('-')) {
-                let operator = this.state.result.indexOf('-')
-                let firstNum = this.state.result.substr(0, operator);
-                let secondNum = this.state.result.substr(operator + 1, this.state.result.length)
-                this.setState({result:Number(firstNum) - Number(secondNum)})
-            }
-            else if  (this.state.result.includes('÷')) {
-                let operator = this.state.result.indexOf('÷')
-                let firstNum = this.state.result.substr(0, operator);
-                let secondNum = this.state.result.substr(operator + 1, this.state.result.length)
-                this.setState({result:Number(firstNum) / Number(secondNum)})
-            }
+            this.operationCheck()
+            this.setState({count:5})
+        } else if (val === '+' || val === '-' || val === '÷') {
+
+            this.setState({ result: this.state.result + val, count: 0 })
         }
         else {
             this.setState({
-                result: this.state.result + val
+                result: this.state.result + val,
+                count: this.state.count + 1
             })
         }
     }
     onChangeHandler = (e) => {
-        this.setState({result:e.target.value})
-    } 
+        this.setState({ result: e.target.value })
+    }
     render() {
         return (
             <div className="container">
-                <input className="calcInput" value={this.state.result} onChange={this.onChangeHandler}/>
+                <input className="calcInput" value={this.state.result} onChange={this.onChangeHandler} />
                 {/* <button onClick={this.handleClick}>3</button> */}
                 <button className="clear" onClick={() => this.handleClick('clear')}>clear</button>
                 <button className="operator " onClick={() => this.handleClick('÷')}>÷</button>
-
                 <button className="buttonCalc" onClick={() => this.handleClick('7')}>7</button>
                 <button className="buttonCalc" onClick={() => this.handleClick('8')}>8</button>
                 <button className="buttonCalc" onClick={() => this.handleClick('9')}>9</button>
