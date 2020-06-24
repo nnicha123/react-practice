a242e792691e5a49e258cbcb6ab587c9ba654744
Nicha Ngamtweerat <nicha_nga@hotmail.com>
2020-06-24 15:33:20 +0700

    added a random quote project

---
+++ react-projects/random-quote/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Quote from './Quote';
+
+function App() {
+  return (
+    <div className="App">
+      <Quote/>
+    </div>
+  );
+}
+
+export default App;
