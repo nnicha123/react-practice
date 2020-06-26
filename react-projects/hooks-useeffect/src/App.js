01661d7110d72494619e77db05e3a81934e99fb7
Nicha Ngamtweerat <nicha_nga@hotmail.com>
2020-06-26 12:46:24 +0700

    renamed folder

--- react-projects/hooks-useeffect/src/App.js
+++ react-projects/hooks-useeffect/src/App.js
@@ -16,7 +16,7 @@ function App() {
       {/* <ClassMouse/> */}
       {/* <HookMouse/> */}
       {/* <MouseContainer/> */}
-      <IntervalClassCounter/>
+      {/* <IntervalClassCounter/> */}
       <IntervalHookCounter/>
     </div>
   );
