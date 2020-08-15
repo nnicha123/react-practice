4c3e256c502649a82689884601a772d23e16e536
Nicha Ngamtweerat <nicha_nga@hotmail.com>
2020-08-15 22:16:09 +0700

    datepicker test

--- react-projects/antd-project/src/App.js
+++ react-projects/antd-project/src/App.js
@@ -1,16 +1,20 @@
 import React from 'react';
-import {Button} from 'antd'
-import { ShoppingFilled,HeartFilled,UserOutlined } from '@ant-design/icons';
+import { Button, DatePicker } from 'antd'
+import { ShoppingFilled, HeartFilled, UserOutlined } from '@ant-design/icons';
 import './App.css'
 import 'antd/dist/antd.css';
+
+function onChange(date) {
+  console.log(date);
+}
 
 function App() {
   return (
     <div className="App">
-
-      <Button  icon={<HeartFilled style={{fontSize:'25px'}}/>} style={{color:'maroon',border:'none'}}></Button>
-      <Button  icon={<ShoppingFilled style={{fontSize:'25px'}}/>} style={{color:'maroon',border:'none'}}></Button>
-      <Button  icon={<UserOutlined style={{fontSize:'25px'}}/>} style={{color:'maroon',border:'none'}}></Button>
+      <DatePicker onChange={onChange} />
+      <Button icon={<HeartFilled style={{ fontSize: '25px' }} />} style={{ color: 'maroon', border: 'none' }}></Button>
+      <Button icon={<ShoppingFilled style={{ fontSize: '25px' }} />} style={{ color: 'maroon', border: 'none' }}></Button>
+      <Button icon={<UserOutlined style={{ fontSize: '25px' }} />} style={{ color: 'maroon', border: 'none' }}></Button>
     </div>
   );
 }
